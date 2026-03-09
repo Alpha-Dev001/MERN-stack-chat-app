ae3d19232938fcfd7c5efd24db55f1e3b6582803
MUNEZERO Alpha <alphamnzr@gmail.com>
2026-03-09 23:15:22 +0200

    started the profile part

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 function ProfilePage() {
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className='min-h-screen bg-cover bg-center flex items-center bg-no-repeat'>
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2
+      border-gray-600 items-center justify-between max-sm:flex-col-reverse rounded-lg'>
+        <form>
+          <h3 className='text-lg'>Profile details</h3>
+        </form>
+        <img src="" alt="" />
+      </div>
     </div>
   )
 }
