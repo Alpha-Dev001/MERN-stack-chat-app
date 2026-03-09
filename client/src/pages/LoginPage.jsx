9da82959419ee1d03a4416aaeaf8d676a5651100
MUNEZERO Alpha <alphamnzr@gmail.com>
2026-03-09 22:28:19 +0200

    added login form

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -8,6 +8,7 @@ function LoginPage() {
   const [email,setEmail] = useState("");
   const [password,setPassword] = useState("");
   const [bio,setBio] = useState("");
+  const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl' style={{ backgroundImage: `url(${assets.bgImage})` }}>
@@ -23,9 +24,35 @@ function LoginPage() {
           <img src={assets.arrow_icon} alt=""  className='w-5 cursor-pointer'/>
           </h2>
 
-        {currState === 'Sign up'} 
+        {currState === 'Sign up' && !isDataSubmitted && (
         <input type="text" className='p-2 border border-gray-500 rounded-md 
         focus:outline-none' placeholder='Full name' required/>
+         )}
+
+         {!isDataSubmitted && (
+          <>
+          <input onChange={(e)=>setEmail(e.target.value)} value={email} 
+           type="email" className='p-2 border border-gray-500 rounded-md 
+          focus:outline-none focus:ring-2 focus:ring-indigo-500' placeholder='Email Address' required/>
+
+          <input onChange={(e)=>setPassword(e.target.value)} value={password} 
+           type="password" className='p-2 border border-gray-500 rounded-md 
+          focus:outline-none focus:ring-2 focus:ring-indigo-500' placeholder='Password' required/>
+          </>
+         )}
+
+         {
+          currState === 'Sign up' && isDataSubmitted && (
+            <textarea onChange={(e)=>setBio(e.target.value)} value={bio}
+            rows={4} className='p-2 border border-gray-500 rounded-md 
+            focus:outline-none focus:ring-2 focus:ring-indigo-500'
+             placeholder='provide a short bio... ' required/>
+          )
+         }
+         <button type='submit' className='py-3 bg-gradient-to-r from-purple-400
+         to-violet-600 text-white rounded-md cursor-pointer'>
+          {currState === "Sign up" ? "Create Account" : "Login Now"}
+         </button>
       </form>
     </div>
   )
